95ee7ed7b3c6b79a66954bd1684e85ab629474b2
Paras <paras.jain2@publicissapient.com>
2025-10-22 06:12:52 +0530

    code updated

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
 import http from "http";
-import { requestHandler } from "./routes";
+import express, { NextFunction, Request, Response } from "express";
 
-const server = http.createServer(requestHandler);
+const app = express();
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log(req.url);
+  console.log(res.statusMessage);
+  next();
+});
+
+const server = http.createServer(app);
 server.listen(3000);
