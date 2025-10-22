11cd75f9034c5026a57fc147b030e537e1eab571
Paras <paras.jain2@publicissapient.com>
2025-10-22 17:09:30 +0530

    code updated

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ app.use(shopRoutes);
 app.use("/admin", adminRoutes);
 
 app.use((_, res:Response) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path:'/' });
 });
 
 app.listen(3000);
