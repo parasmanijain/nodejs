7cb63e5553d7f3c4c28056eddbaffd7e48b958c1
Paras <paras.jain2@publicissapient.com>
2025-10-22 06:39:31 +0530

    code updated

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,22 @@
 import express, { NextFunction, Request, Response } from "express";
+import bodyParser from "body-parser";
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/add-product', (_, res:Response) => {
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+app.use("/add-product", (_, res: Response) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  );
 });
 
-app.post('/product', (req:Request, res:Response) => {
-    console.log(req.body);
-    res.redirect('/');
+app.post("/product", (req: Request, res: Response) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
-app.use('/', (_, res:Response) => {
-  res.send('<h1>Hello from Express!</h1>');
+app.use("/", (_, res: Response) => {
+  res.send("<h1>Hello from Express!</h1>");
 });
 
 app.listen(3000);
