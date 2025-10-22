edb6ce42b0f2796ec34ea4b4ad3c1257a352f1e8
Paras <paras.jain2@publicissapient.com>
2025-10-22 20:40:04 +0530

    Code updated

--- src/controllers/error.ts
+++ src/controllers/error.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
 
 export const get404 = (_: Request, res: Response) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+  res.status(404).render("404", { pageTitle: "Page Not Found", path: "/" });
 };
