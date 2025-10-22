79cfd428ac3a6aca77fab1e4a1e085976892bbab
Paras <paras.jain2@publicissapient.com>
2025-10-22 08:54:52 +0530

    code updated

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,13 +1,15 @@
 import express, { Request, Response } from "express";
+import path from 'path';
 
 export const router = express.Router();
 
+// /admin/add-product => GET
 router.get("/add-product", (_, res: Response) => {
-  res.send(
-    '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-  );
+   res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+ 
 });
 
+// /admin/add-product => POST
 router.post("/add-product", (req: Request, res: Response) => {
   console.log(req.body);
   res.redirect("/");
--- src/routes/shop.ts
+++ src/routes/shop.ts
@@ -1,7 +1,8 @@
 import express, { Response } from "express";
+import path from 'path';
 
 export const router = express.Router();
 
 router.get('/', (_, res:Response) => {
-  res.send('<h1>Hello from Express!</h1>');
+  res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
