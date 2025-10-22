35766b3dc7db9dbea49261110a444432adc09f10
Paras <paras.jain2@publicissapient.com>
2025-10-22 06:26:14 +0530

    code updated

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,19 @@
 import express, { NextFunction, Request, Response } from "express";
 
 const app = express();
-app.use((req: Request, res: Response, next: NextFunction) => {
-  console.log(req.url);
-  console.log(res.statusMessage);
-  next();
+app.use('/', (req:Request, res:Response, next:NextFunction) => {
+    console.log('This always runs!');
+    next();
 });
 
-app.use((req: Request, res: Response) => {
-  res.send('<h1>Hello from Express!</h1>')
+app.use('/add-product', (req:Request, res:Response) => {
+  console.log('In add product route middleware!');
+  res.send('<h1>The "Add Product" Page</h1>');
+});
+
+app.use('/', (req:Request, res:Response) => {
+  console.log('In another middleware!');
+  res.send('<h1>Hello from Express!</h1>');
 });
 
 app.listen(3000);
