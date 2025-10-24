b4740741a5172e752ec40b30a36a12e40443aff6
Paras <paras.jain2@publicissapient.com>
2025-10-24 15:42:30 +0530

    Code updated

--- src/controllers/admin.ts
+++ src/controllers/admin.ts
@@ -12,7 +12,8 @@ export const getAddProduct = (_: Request, res: Response) => {
 };
 
 export const postAddProduct = (req: Request, res: Response) => {
-  const product = new Product(req.body.title);
+  const {title, imageUrl, description, price} = req.body;
+  const product = new Product(title, imageUrl, description, price);
   product.save();
   res.redirect("/");
 };
